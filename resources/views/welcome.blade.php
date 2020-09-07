5741e2dcf27a6bca30f125df70246bd08183f5d6
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2020-09-08 00:40:43 +0100

    linking external files & navbar
    linked font awesome , css and js
    navbar responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,88 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PortFolio XD</title>
 
         <!-- Fonts -->
+        <script src="https://kit.fontawesome.com/5f7e84bf6f.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="C:\Users\Youcode\Desktop\portfolio\resources\style.css">
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <link href="{{ asset('/css/css.css') }}" rel="stylesheet"> 
+       
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+    
+{{-- start of navbar  --}}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+<nav>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+
+    <section class="aw-header bg-white border-b border-gray-200 shadow-md">
+      <header class="container mx-auto flex items-center justify-between flex-wrap px-2 py-4 lg:p-6 ">
+        <div id="mobile-menu__toggler" class="block lg:hidden">
+          <button id="mobile-menu__open" class="px-3 py-1 w-20 border rounded text-white bg-purple-700 border-transparent hover:bg-purple-900 transition">
+                  Menu
+              </button>
+          <button id="mobile-menu__close" class="hidden px-3 py-1 w-20 border rounded text-white bg-purple-700 border-transparent hover:bg-purple-900 transition">
+                  Close
+              </button>
         </div>
+  
+        <div class="aw-header__logo flex items-center flex-shrink-0 text-purple-700 hover:text-purple-900 mx-2 lg:mr-6 order-1 transition ">
+          <span>&#9760;</span>
+
+
+          <span class="font-semibold text-lg tracking-tight">PortfoLio</span>
+        </div>
+  
+        <nav id="mobile-menu__items" class="aw-header__nav hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto order-3 lg:order-2">
+          <ol class="text-lg lg:flex-grow lg:flex lg:justify-around">
+            <li class="block mb-4 mt-3 lg:inline-block lg:mb-0 lg:mt-0 mr-4">
+              <a href="#responsive-header" class="text-gray-700 hover:text-gray-900 transition">About</a>
+            </li>
+  
+            <li class="block mb-4 lg:inline-block lg:mb-0 mr-4">
+              <a href="#responsive-header" class="text-gray-700 hover:text-gray-900 transition">Services</a>
+            </li>
+  
+  
+            <li class="block mb-4 lg:inline-block lg:mb-0 mr-4">
+              <a href="#responsive-header" class="text-gray-700 hover:text-gray-900 transition">Products & Testimonials</a>
+            </li>
+  
+            <li class="block lg:inline-block lg:mb-0 mr-4">
+              <a href="#responsive-header" class="text-gray-700 hover:text-gray-900 transition">Contact</a>
+            </li>
+          </ol>
+        </nav>
+  
+        <div class="flex order-2">
+  
+          
+          </div>
+        </div>
+      </header>
+    </section>
+
+
+</nav>
+
+   {{-- the end of navbar with toggle --}}
+    <body>
+
+
+        
+        
+
+        
     </body>
+
+
+
+    <footer>
+        <script src="{{ asset('js/js.js') }}" ></script>
+    </footer>
 </html>
