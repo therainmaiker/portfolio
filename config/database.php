626eb8d35046c501e7a871de1e5f66f307499555
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2020-09-08 15:35:52 +0100

    db connection
    database connection & create
    create superadmin model

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('moja', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,10 +46,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'moja'),
+            'username' => env('DB_USERNAME', 'mojauser'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,9 +78,117 @@
 
 
         
-        
+      <div class="relative pt-16 pb-32 flex content-center items-center justify-center" style="min-height: 75vh;">
+        <div class="absolute top-0 w-full h-full bg-center bg-cover background-img"
+        >
+          <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>  
+          {{-- oppacity image dark --}}
+        </div>
+        <div class="container relative mx-auto">
+          <div class="items-center flex flex-wrap">
+            <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+              <div class=" text-about-me">
+                <h1 class="text-white font-semibold text-5xl">
+                  Your story starts with us.
+                </h1>
+                <p class="mt-4 text-lg text-gray-300">
+                  I am a web developer from Morocco . I enjoy building everything from small business sites to rcih interactive web apps ,
+                   if you are a business seeking a web presence or an employer looking to hire n you ca get in touch with me here.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden" style="height: 70px; transform: translateZ(0px);">
+          <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+            <polygon class="text-gray-300 fill-current" points="2560 0 2560 100 0 100"></polygon>
+          </svg>
+        </div>
+      </div>
 
-        
+
+<section class=" my-hub ">
+
+
+  <div class="flex flex-wrap">
+
+
+
+
+
+    <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-6 text-center">
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+        <div class="px-4 py-5 flex-auto">
+          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+            <i class="fas fa-award"></i>
+          </div>
+          <h6 class="text-xl font-semibold">Experience</h6>
+          <h5 class="text-l font-semibold">Infographic Design</h5>
+          <p class="mt-2 mb-4 text-gray-600">
+            working on the creation of infographics and data visualisation projects. we creates data-driven visual content and reports for consumers
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="w-full md:w-4/12 px-6 text-center">
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+        <div class="px-4 py-5 flex-auto">
+          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
+            <i class="fas fa-retweet"></i>
+          </div>
+          <h6 class="text-xl font-semibold">Education</h6>
+          <h5 class="text-l font-semibold">Youcode University</h5>
+          <p class="mt-2 mb-4 text-gray-600">
+            responsible for server-side web application logic and integration of the work front-end developers do.
+             Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers
+
+
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    <div class="pt-6 w-full md:w-4/12 px-6 text-center">
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+        <div class="px-4 py-5 flex-auto">
+          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+            <i class="fas fa-fingerprint"></i>
+          </div>
+          <h6 class="text-xl font-semibold">About me</h6>
+          <h5 class="text-l font-semibold">Who AM I?</h5>
+          <p class="mt-2 mb-4 text-gray-600">
+            Well-organised person, problem solver, independent employee with high attention to detail. Fan of MMA, outdoor activities, 
+            TV series and, recently, English literature. A family person, father of two fractious boys, therefore remote employment is preferred.
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+    
+
+
+
+
+
+
+</section>
+     
     </body>
 
 
